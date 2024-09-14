bcb9ce721ceda1c1ba40f2a74ee2a0b09b87d84a
Jhmeel <muhammadjhmeel01@gmail.com>
2024-09-14 15:55:57 +0100

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -6,26 +6,50 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 export default defineConfig({
   plugins: [
     react(),
-    commonjs(),
+    commonjs({
+      requireReturnsDefault: 'auto',
+      include: [
+        /node_modules/,
+        /\/node_modules\/@tbdex\/http-client/,
+        /\/node_modules\/@web5\/dids/,
+      ],
+    }),
     nodePolyfills({
       protocolImports: true,
     }),
-  
   ],
   resolve: {
     alias: {
       util: "util",
       events: "events",
       stream: "stream-browserify",
+      crypto: "crypto-browserify",
+      http: "stream-http",
+      https: "https-browserify",
+      buffer: "buffer",
     },
   },
   build: {
     commonjsOptions: {
       transformMixedEsModules: true,
     },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'ethers', '@tbdex/http-client', '@web5/dids'],
+        },
+      },
+    },
   },
   define: {
     "process.env": {},
     global: "globalThis",
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'ethers', '@tbdex/http-client', '@web5/dids'],
+    esbuildOptions: {
+      target: 'esnext',
+      supported: { bigint: true },
+    },
   },
 });
