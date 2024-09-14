85ce25bb5546a69d3847933adb2759b4907208f7
Jhmeel <muhammadjhmeel01@gmail.com>
2024-09-14 16:36:37 +0100

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,32 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import commonjs from "@rollup/plugin-commonjs";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { visualizer } from "rollup-plugin-visualizer";
+import { babel } from '@rollup/plugin-babel';
 
 export default defineConfig({
   plugins: [
     react(),
+    babel({
+      babelHelpers: 'bundled',
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+      plugins: ['@babel/plugin-transform-runtime'],
+      exclude: 'node_modules/**',
+    }),
     commonjs({
       requireReturnsDefault: 'auto',
       include: [
         /node_modules/,
         /\/node_modules\/@tbdex\/http-client/,
         /\/node_modules\/@web5\/dids/,
+        /\/node_modules\/multiformats/,
       ],
+      transformMixedEsModules: true,
     }),
     nodePolyfills({
       protocolImports: true,
     }),
+    visualizer(),
   ],
   resolve: {
     alias: {
@@ -27,19 +38,31 @@ export default defineConfig({
       http: "stream-http",
       https: "https-browserify",
       buffer: "buffer",
+      process: "process/browser",
     },
   },
   build: {
-    rollupOptions: {
-      plugins: [commonjs()],
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      include: [/node_modules/],
     },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'ethers'],
+          tbdex: ['@tbdex/http-client'],
+          web5: ['@web5/dids'],
+        },
+      },
+    },
+    sourcemap: true,
   },
   define: {
-    "process.env": {},
-    global: "globalThis",
+    'process.env': {},
+    global: 'globalThis',
   },
   optimizeDeps: {
-    include: ['react', 'react-dom', 'ethers', '@tbdex/http-client', '@web5/dids'],
+    include: ['react', 'react-dom', 'ethers', '@tbdex/http-client', '@web5/dids', 'multiformats'],
     esbuildOptions: {
       target: 'esnext',
       supported: { bigint: true },
