f522bb131821d8d20973b40958384c8e6d3f28f3
Jhmeel <muhammadjhmeel01@gmail.com>
2024-09-15 16:10:59 +0100

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -3,27 +3,19 @@ import react from "@vitejs/plugin-react-swc";
 import commonjs from "@rollup/plugin-commonjs";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import { visualizer } from "rollup-plugin-visualizer";
-import { babel } from '@rollup/plugin-babel';
+import { babel } from "@rollup/plugin-babel";
 
 export default defineConfig({
   plugins: [
     react(),
     babel({
-      babelHelpers: 'bundled',
-      presets: ['@babel/preset-env', '@babel/preset-react'],
-      plugins: ['@babel/plugin-transform-runtime'],
-      exclude: 'node_modules/**',
+      babelHelpers: "bundled",
+      presets: ["@babel/preset-env", "@babel/preset-react"],
+      plugins: ["@babel/plugin-transform-runtime"],
+      exclude: "node_modules/**",
     }),
     commonjs({
-      requireReturnsDefault: 'auto',
-      include: [
-        /node_modules/,
-        /\/node_modules\/@tbdex\/http-client/,
-        /\/node_modules\/@web5\/dids/,
-        /\/node_modules\/multiformats/,
-        /\/node_modules\/string.prototype.matchall/,
-        /\/node_modules\/level-transcoder/,
-      ],
+      requireReturnsDefault: "auto",
       transformMixedEsModules: true,
     }),
     nodePolyfills({
@@ -46,28 +38,17 @@ export default defineConfig({
   build: {
     commonjsOptions: {
       transformMixedEsModules: true,
-
     },
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          tbdex: ['@tbdex/http-client'],
-          web5: ['@web5/dids'],
-          levelTranscoder:['level-transcoder'],
 
-        },
-      },
-    },
     sourcemap: true,
   },
   define: {
-    'process.env': {},
-    global: 'globalThis',
+    "process.env": {},
+    global: "globalThis",
   },
   optimizeDeps: {
-    include: ['level-transcoder','string.prototype.matchall', '@tbdex/http-client', '@web5/dids'],
     esbuildOptions: {
-      target: 'esnext',
+      target: "esnext",
       supported: { bigint: true },
     },
   },
