3da7ea23cea2a0440637d86bd71deb1cecfd8c54
Jhmeel <muhammadjhmeel01@gmail.com>
2024-09-15 17:39:09 +0100

    updates

--- esbuild.config.js
+++
@@ -1,8 +0,0 @@
-import esbuild from 'esbuild'
-
-esbuild.build({
-  entryPoints: ['src/main.tsx'], // Adjust to your entry file
-  bundle: true,
-  outfile: 'dist/bundle.js', // Output file
-  minify: true,
-}).catch(() => process.exit(1));
